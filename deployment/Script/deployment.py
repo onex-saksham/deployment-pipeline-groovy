9e3f291498baec0b02db6e81ca22e8c3583209fd
Saksham Jain <saksham.jain@onextel.com>
2025-11-11 18:52:05 +0530

    dep

--- deployment/Script/deployment.py
+++ deployment/Script/deployment.py
@@ -876,7 +876,7 @@ def deploy_api(config, binary_path, ssh):
                 # Copy env file
                 with SCPClient(ssh.get_transport()) as scp:
                     scp.put(f"{binary_path}/Setups/api/{env_file_name}", f"{deployment_path}/api/")
-                run(ssh, f"cd {deployment_path}/api && mv {env_file_name} {env_file_versioned}")
+                # run(ssh, f"cd {deployment_path}/api && mv {env_file_name} {env_file_versioned}")
 
                 # Copy log4j file
                 with SCPClient(ssh.get_transport()) as scp:
